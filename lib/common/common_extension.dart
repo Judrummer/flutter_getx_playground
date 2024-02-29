260ddff54ec956ca4f262bf6037d059d1633017f
Ju Tipatai Puthanukunkit <judrummercpe@gmail.com>
2024-02-29 16:45:35 +0700

    feat: add measureTime function

--- lib/common/common_extension.dart
+++ lib/common/common_extension.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 extension CommonExtension<T> on T {
   U let<U>(U Function(T it) action) {
     return action(this);
@@ -15,4 +17,12 @@ extension CommonExtension<T> on T {
       return null;
     }
   }
+}
+
+Future<T> measureTime<T>(FutureOr<T> Function() runner) async {
+  final startTime = DateTime.timestamp();
+  final result = await runner();
+  final endTime = DateTime.timestamp();
+  print('$result in ${endTime.difference(startTime).inMilliseconds} millisecs');
+  return result;
 }
