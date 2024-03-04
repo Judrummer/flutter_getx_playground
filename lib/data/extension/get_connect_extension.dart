fc3b74f26ae9e1f36213d697d72be9b5aab9b5f8
Ju Tipatai Puthanukunkit <judrummercpe@gmail.com>
2024-03-04 16:07:37 +0700

    feat: update deps

--- lib/data/extension/get_connect_extension.dart
+++ lib/data/extension/get_connect_extension.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:get/get_connect/connect.dart';
 
+typedef DecodeJsonIsolateParam<T> = ({T Function(Map<String, dynamic>) fromJson, dynamic body});
+Future<T> decodeJsonIsolate<T>(DecodeJsonIsolateParam<T> param) async {
+  return param.fromJson(param.body);
+}
+
 extension GetResponseExtension on Future<Response> {
-  Future<T> decodeJson<T>(T Function(Map<String, dynamic>) fromJson) => then((value) => fromJson(value.body));
+  Future<T> decodeJson<T>(T Function(Map<String, dynamic>) fromJson) =>
+      then((value) => compute(decodeJsonIsolate, (fromJson: fromJson, body: value.body)));
 
   Future<List<T>> decodeJsonList<T>(
     T Function(Map<String, dynamic>) fromJson,
